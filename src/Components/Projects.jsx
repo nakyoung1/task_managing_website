6b5eab93442ef1b566c91bd4feaac41dc2814921
nakyoung1 <skrud5730@naver.com>
2025-04-10 13:32:02 +0900

    home 추가

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,12 @@
 function Projects({ projects, handleClick, handleShowProject }) {
      return (
           <nav className="flex flex-col items-center gap-7 bg-gray-700 rounded-tr-lg h-[100vh] w-[300px] m text-white  ">
-               <h3 className=" font-bold mt-15 text-xl">YOUR PROJECTS</h3>
+               <h3
+                    onClick={handleHome}
+                    className=" font-bold mt-15 text-xl cursor-pointer"
+               >
+                    YOUR PROJECTS
+               </h3>
                <button
                     onClick={handleClick}
                     className="bg-gray-500 p-2 rounded cursor-pointer hover:bg-gray-200 hover:text-black"
